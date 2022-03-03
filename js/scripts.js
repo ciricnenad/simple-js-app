fb8615cdcb5e8dc361738123d2eaf1758854ea5c
ciricnenad <91833550+ciricnenad@users.noreply.github.com>
2022-03-03 12:05:36 +0100

    created a new variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,39 @@
-alert('Hello world');
-let favoriteFood = 'cheeseburger';
-document.write(favoriteFood);
+//pokemon:
+let pokemonList = [{
+  name: 'Bulbasaur',
+  height: 0.7,
+  weight: 6.9,
+  type: ['grass', 'poison']
+},
+
+{
+  name: 'Snorlax',
+  height: 2.1,
+  weight: 460,
+  type: ['normal']
+},
+
+{
+  name: 'Mr. Mime',
+  height: 1.3,
+  weight: 54.5,
+  type: ['psychic', 'fairy']
+},
+
+{
+  name: 'Ho-oh',
+  height: 3.8,
+  weight: 199,
+  type: ['fire', 'flying']
+},
+
+{
+  name: 'Horsea',
+  height: 0.4,
+  weight: 8,
+  type: ['water']
+},
+];
+
+document.write(pokemonList);
+
