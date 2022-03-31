efd37863ee8b6669c38053990b992ba67437e9b2
ciricnenad <91833550+ciricnenad@users.noreply.github.com>
2022-03-31 10:00:17 +0200

    added new functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,79 @@
 //pokemon:
 let pokemonRepository = (function () {
   let pokemonList = [{
-  name: 'Bulbasaur',
-  height: 0.7,
-  weight: 6.9,
-  type: ['grass', 'poison']
-},
+    name: 'Bulbasaur',
+    height: 0.7,
+    weight: 6.9,
+    type: ['grass', 'poison']
+  },
 
-{
-  name: 'Snorlax',
-  height: 2.1,
-  weight: 460,
-  type: ['normal']
-},
+  {
+    name: 'Snorlax',
+    height: 2.1,
+    weight: 460,
+    type: ['normal']
+  },
 
-{
-  name: 'Mr. Mime',
-  height: 1.3,
-  weight: 54.5,
-  type: ['psychic', 'fairy']
-},
+  {
+    name: 'Mr. Mime',
+    height: 1.3,
+    weight: 54.5,
+    type: ['psychic', 'fairy']
+  },
 
-{
-  name: 'Ho-oh',
-  height: 3.8,
-  weight: 199,
-  type: ['fire', 'flying']
-},
+  {
+    name: 'Ho-oh',
+    height: 3.8,
+    weight: 199,
+    type: ['fire', 'flying']
+  },
 
-{
-  name: 'Horsea',
-  height: 0.4,
-  weight: 8,
-  type: ['water']
-},
+  {
+    name: 'Horsea',
+    height: 0.4,
+    weight: 8,
+    type: ['water']
+  },
 ];
 
 function add(pokemon) {
-    pokemonList.push(pokemon);
-  }
+  pokemonList.push(pokemon);
+}
 
-  function getAll() {
-    return pokemonList;
-  }
+function getAll() {
+  return pokemonList;
+}
 
-  return {
-    add: add,
-    getAll: getAll
-  };
+
+function showDetails (pokemon) {
+  console.log(pokemon.name)
+}
+
+function addListener (button, pokemon) {
+  button.addEventListener ("click", function () {
+    showDetails(pokemon)
+  })
+}
+
+function addListItem(pokemon) {
+  let pokemonList = document.querySelector(".pokemon-list");
+  let listPokemon = document.createElement("li");
+  let button = document.createElement("button");
+  button.innerText = pokemon.name;
+  button.classList.add("button-class");
+  listPokemon.appendChild(button);
+  pokemonList.appendChild(listPokemon);
+  addListener(button, pokemon);
+}
+
+return {
+  add: add,
+  getAll: getAll,
+  addListItem: addListItem,
+  showDetails: showDetails
+}
 })();
 
-pokemonRepository.getAll().forEach(function(pokemon){
-  if (pokemon.height > 2) {
-  document.write(pokemon.name + " (height): " + pokemon.height + " - Wow that's big! <br/>");
-  } else {
-  document.write(pokemon.name + " (height): " + pokemon.height + ", <br/>");
-  }
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon)
 });
-
-console.log(pokemonRepository.getAll());
-pokemonRepository.add({ name: 'Pikachu' });
-console.log(pokemonRepository.getAll());
